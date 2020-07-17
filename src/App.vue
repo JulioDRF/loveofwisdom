f26f425604cd5bb0ea0708664d0a2cdef6eb918f
julio <juliod.rangelfarell@gmail.com>
2020-07-17 05:17:13 -0400

    Wire main app file and js

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,65 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" :style="injectedStyle">
+    <TheNavBar/>
+    <router-view class="nav-padding"/>
+    <TheFooter/>
   </div>
 </template>
 
+<script>
+import TheFooter from '@/components/TheFooter.vue'
+import TheNavBar from '@/components/TheNavBar.vue'
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  name: 'App',
+  components: {
+    TheFooter,
+    TheNavBar
+  },
+  computed: {
+    injectedStyle () {
+      const textColor = this.darkModeEnabled ? '#ffffff' : '#313d4f'
+      const bgColor = this.darkModeEnabled ? '#222c3c' : '#ffffff'
+      const bgColorSecondary = this.darkModeEnabled ? '#343a40' : '#f8f9fa'
+      return {
+        '--text-color': textColor,
+        '--bg-color': bgColor,
+        '--bg-color-secondary': bgColorSecondary
+      }
+    },
+    ...mapState([
+      'darkModeEnabled'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'fetchCategories',
+      'fetchEntries'
+    ])
+  },
+  mounted () {
+    this.fetchCategories()
+    this.fetchEntries()
+  }
+}
+</script>
+
 <style>
+
+html {
+  scroll-behavior: smooth;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: var(--text-color);
+  background: var(--bg-color);
+  transition-duration: 0.25s;
+  transition-timing-function: ease-in;
+  font-family: Roboto, Arial, Helvetica, sans-serif;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.nav-padding {
+  padding-top: 5rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import './plugins/vue-select'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import 'typeface-roboto/index.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
