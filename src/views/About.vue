7fecec608dff2244a35bb6e13f0476bb6471af36
julio <juliod.rangelfarell@gmail.com>
2020-07-18 14:02:25 -0400

    Add more content to About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="not-found">
     <b-container fluid="md" class="m-auto">
-      <b-row align="center">
+      <b-row align="center" class="mb-4">
         <b-col cols="auto" class="m-auto">
           <h1>About Love of Wisdom</h1>
         </b-col>
       </b-row>
       <b-row align="left">
         <b-col cols="auto" class="m-auto">
-          <p>Love of Wisdom is a website that categorizes all the entries within the
+          <p><b>Philosophy</b> comes from the Greek word <i>philosophia</i>, which combines the words
+          <i>philo</i> meaning "love of" and <i>sophia</i> meaning "wisdom."
+          </p>
+          <p>
+            Love of Wisdom is a website that categorizes all the entries within the
             <b-link target="_blank" href="https://plato.stanford.edu/">Stanford Encyclopedia of Philosphy</b-link>
             and provides a snappy and effective way of finding what you're looking for
           </p>
